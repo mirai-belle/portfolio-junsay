ac5b62f647f2531ac233a77a76c550aff09073ea
Myrrhea Belle <mbelle.junsay@gmail.com>
2025-09-17 11:02:12 +0800

    add Footer component with styles; implement footer layout and copyright text

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+import '../../Global.css'
+function Footer() {
+  return (
+    <div className="footer">
+        <p>&copy; Myrr Junsay, 2025</p>
+    </div>
+  )
+}
+
+export default Footer
